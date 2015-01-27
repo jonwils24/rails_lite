75a2eeece5a9abcdbec34a0b187cca65fa56d2b1
Jonathan Wilson <wilsonwjonathan@gmail.com>
2015-01-26 21:27:13 -0800

    comment clean up

--- lib/phase5/params.rb
+++ lib/phase5/params.rb
@@ -2,7 +2,7 @@ require 'uri'
 
 module Phase5
   class Params
-    # use your initialize to merge params from
+    # use initialize to merge params from
     # 1. query string
     # 2. post body
     # 3. route params
@@ -29,10 +29,10 @@ module Phase5
     class AttributeNotFoundError < ArgumentError; end;
 
     private
-    # this should return deeply nested hash
+    # return deeply nested hash
     # argument format
     # user[address][street]=main&user[address][zip]=89436
-    # should return
+    # will return
     # { "user" => { "address" => { "street" => "main", "zip" => "89436" } } }
     def parse_www_encoded_form(www_encoded_form)
       params = {}
@@ -54,8 +54,9 @@ module Phase5
       params
     end
 
-    # this should return an array
-    # user[address][street] should return ['user', 'address', 'street']
+    # return an array
+    # user[address][street] 
+    # will return ['user', 'address', 'street']
     def parse_key(key)
       key.split(/\[|\]\[|\]/)
     end
